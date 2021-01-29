8e96478faac4339f1224c58b62b366eadb835ae8
Richard <richou6@gmail.com>
2021-01-29 17:57:22 +0100

    Change mocha to jest

--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -1,8 +1,7 @@
-const assert = require('assert');
 const fizzbuzz = require('./fizzbuzz');
 
 describe('fizzbuzz test', () => {
   it('should hello size is 5', () => {
-    assert.equal('hello'.length, 5)
+    expect('hello').toHaveLength(5);
   })
 })
